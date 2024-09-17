d5feb4095752cddc3d66e01fd4543e800d5c7ba9
Srihari <120706989+Srihari1708@users.noreply.github.com>
2024-09-17 14:17:07 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -3,6 +3,10 @@
 
 TEST(StringCalculatorTestSuite,Add_emptyInputString_ZeroIsExpected){
   //Arrange
+  string input = "";
+  int expectedValue=0;
   //Act
+  int actualValue=Add(input);
   //Assert
+  ASSERT_EQ(actualValue,expectedValue);
 }
