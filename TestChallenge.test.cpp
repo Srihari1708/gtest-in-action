d308e0db7bd0e710c4e2458ca8e0a0799c8869ec
Srihari <120706989+Srihari1708@users.noreply.github.com>
2024-09-18 18:00:46 +0530

    Update TestChallenge.test.cpp

--- TestChallenge.test.cpp
+++ TestChallenge.test.cpp
@@ -25,7 +25,7 @@ TEST(NetworkAlerterTestSuite,StateBasedTest){
 TEST(NetworkAlerterTestSuite,InteractionTest){
     NetworkAlerterMock mock;
       //Expected Value
-     EXPECT_CALL(mock,alert(FloatEq(204.444))).WillOnce(Return(500));
+     EXPECT_CALL(mock,alert(FloatEq(204.444f))).WillOnce(Return(500));
     int failureCount =  alertInCelciusFacade(400,&mock);
     ASSERT_EQ(failureCount,1);
       
