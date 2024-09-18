ac1a917a7ab8b862a8a8e4df2532b45137bb3a70
Srihari <120706989+Srihari1708@users.noreply.github.com>
2024-09-18 11:31:13 +0530

    Update ValueParameterizationTest.cpp

--- ValueParameterizationTest.cpp
+++ ValueParameterizationTest.cpp
@@ -7,27 +7,36 @@ protected:
     string input;
     int expectedValue;
     int actualValue;
+
+    // Setting up the test case
+    void SetUp() override {
+        // This will be overridden in the parameterized test
+        input = "";
+        expectedValue = 0;
+    }
 };
 
-class StringCalculatorParameterFixture:public StringCalculatorTestFixture, public testing::WithParamInterface<tuple<string,int>>{
-
+class StringCalculatorParameterFixture : public StringCalculatorTestFixture,
+                                        public testing::WithParamInterface<tuple<string, int>> {
+protected:
+    void SetUp() override {
+        // Set up parameters for the test based on the current test case
+        StringCalculatorTestFixture::SetUp(); // Call the base setup
+        input = std::get<0>(GetParam());
+        expectedValue = std::get<1>(GetParam());
+    }
 };
 
+// Parameter Values for testing
+INSTANTIATE_TEST_SUITE_P(ValidStringCalculatorInputs, StringCalculatorParameterFixture,
+                         testing::Values(
+                             make_tuple("", 0),
+                             make_tuple("1", 1),
+                             make_tuple("1,2", 3),
+                             make_tuple("1,1000", 1001)
+                         ));
 
-//Parameter Values
-INSTANTIATE_TEST_SUITE_P(ValidStringCalculatorInputs,StringCalculatorParameterFixture,testing::Values(
-  make_tuple("",0),
-  make_tuple("1",0),
-  make_tuple("1",1),
-  make_tuple("1,2",3),
-  make_tuple("1,1000",1001)  
-));
-
-TEST_P(StringCalculatorParameterFixture, ParameterizedTest){
-   input = std::get<0>(GetParam());
-  expectedValue = std::get<1>(GetParam());
-  actualValue = Add(input);
-  ASSERT_EQ(actualValue,expectedValue);
-
-
+TEST_P(StringCalculatorParameterFixture, ParameterizedTest) {
+    actualValue = Add(input);
+    ASSERT_EQ(actualValue, expectedValue);
 }
