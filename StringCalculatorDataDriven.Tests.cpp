5258c98d685b9b3348b3db0aebc3d91d1f1ce6b7
Srihari <120706989+Srihari1708@users.noreply.github.com>
2024-09-18 09:58:22 +0530

    Update StringCalculatorDataDriven.Tests.cpp

--- StringCalculatorDataDriven.Tests.cpp
+++ StringCalculatorDataDriven.Tests.cpp
@@ -10,31 +10,6 @@ public:
       }
 };
 
-class StringCalculatorDataDrivenFixture:public testing::Test{
-
-protected:
-  vector<TestDataPair*> dataList;
-
-   //Before Each Test Case
-  void SetUp(){
-      dataList.push_back(new TestDataPair { "",0});
-      dataList.push_back(new TestDataPair { "0",0});
-      dataList.push_back(new TestDataPair { "1",1});
-      dataList.push_back(new TestDataPair { "1,2",3});
-      dataList.push_back(new TestDataPair { "1,2,3",6});
-  }
-  // After Each Test Case
-void TearDown(){
-    
-}    
-};
-
-TEST_F(StringCalculatorDataDrivenFixture,DataDrivenTestCase){
-     for (TestDataPair* dataPairPtr : dataList) {
-        int actualValue=Add(dataPairPtr->input);
-        ASSERT_EQ(actualValue,dataPairPtr->expectedValue);
-    }
-}
 TEST(StringCalculatorDataDrivenTestSuite,DataDrivenTestCase){
 
   TestDataPair pair_one { "",0};
