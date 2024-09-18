eebe8251231db41c1fb1979a7e4ae990694baab0
Srihari <120706989+Srihari1708@users.noreply.github.com>
2024-09-18 10:00:50 +0530

    Update StringCalculatorDataDriven.Tests.cpp

--- StringCalculatorDataDriven.Tests.cpp
+++ StringCalculatorDataDriven.Tests.cpp
@@ -10,8 +10,11 @@ public:
       }
 };
 
-TEST(StringCalculatorDataDrivenTestSuite,DataDrivenTestCase){
 
+
+
+TEST(StringCalculatorDataDrivenTestSuite,DataDrivenTestCase){
+  vector<TestDataPair> dataList;
   TestDataPair pair_one { "",0};
   TestDataPair pair_two { "0",0};
   TestDataPair pair_three { "1",1};
@@ -33,3 +36,33 @@ TEST(StringCalculatorDataDrivenTestSuite,DataDrivenTestCase){
 }
 
 
+
+class StringCalculatorTestFixture : public ::testing::Test {
+protected:
+    struct TestDataPair {
+        std::string input;
+        int expectedValue;
+
+        TestDataPair(std::string _input, int _expectedValue)
+            : input{_input}, expectedValue{_expectedValue} {}
+    };
+
+    std::vector<TestDataPair> dataList;
+
+    void SetUp() override {
+        // Initialize the test data
+        dataList.push_back(TestDataPair("", 0));
+        dataList.push_back(TestDataPair("0", 0));
+        dataList.push_back(TestDataPair("1", 1));
+        dataList.push_back(TestDataPair("1,2", 3));
+        dataList.push_back(TestDataPair("1,2,3", 6));
+    }
+};
+
+TEST_F(StringCalculatorTestFixture, DataDrivenTestCase) {
+    // Iterate using a range-based for-loop
+    for (const TestDataPair& dataPair : dataList) {
+        int actualValue = Add(dataPair.input);
+        ASSERT_EQ(actualValue, dataPair.expectedValue);
+    }
+}
