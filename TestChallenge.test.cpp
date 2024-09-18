2e175e6fb18a5f40e698a36d23d43968b0bce128
Srihari <120706989+Srihari1708@users.noreply.github.com>
2024-09-18 17:10:48 +0530

    Update TestChallenge.test.cpp

--- TestChallenge.test.cpp
+++ TestChallenge.test.cpp
@@ -1,2 +1,16 @@
 #include <Testchallenge.h>
 #include <gtest/gtest.h>
+
+
+//stub - Fake dependency
+class NetworkAlerterStub:public INetworkAlerter{
+public:
+    int alert(float celcius) override{
+        return 500;
+    }
+};
+TEST(NetworkAlerterTestSuite,StateBasedTest){
+  NetworkAlerterStub stub;
+  int failurecount = int alertInCelsiusFacade(400,&stub);  
+  ASSERT_EQ(failurecount,1);
+}
