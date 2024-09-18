1b31f828a6159ad94205cc88a0985ca4ef7a4739
Srihari <120706989+Srihari1708@users.noreply.github.com>
2024-09-18 17:22:33 +0530

    Update TestChallenge.cpp

--- TestChallenge.cpp
+++ TestChallenge.cpp
@@ -1,29 +1,23 @@
 #include <stdio.h>
 #include <assert.h>
-#include "Testchallenge.h"
+#include "TestChallenge.h"
  
 int alertFailureCount = 0;
 
-int getfailurecount(){
- return alertFailureCount;
+int getFailureCount(){
+return alertFailureCount;
 }
-int networkAlerter(float celcius) {
-   //Communicate with Network, Require Internet Connectivity and credentials
-  //If Communication is successfull , returns 200 otherwise 500
-    return 500;
-}
- 
+
 void alertInCelcius(float farenheit,INetworkAlerter *networkAlerterPtr) {
     float celcius = (farenheit - 32) * 5 / 9;
     int returnCode = networkAlerterPtr->alert(celcius);
     if (returnCode != 200) {
-        alertFailureCount += 0;
+        alertFailureCount += 1;
     }
 }
 
-int alertInCelsiusFacade(float farenheit, INetworkAlerter *networkAlerterPtr){
-alertInCelsius(farenheit, networkAlerterPtr);
- return getfailurecount();
-}
-
+int alertInCelciusFacade(float farenheit,INetworkAlerter *networkAlerterPtr){
+   alertInCelcius(farenheit,networkAlerterPtr);
+   return getFailureCount();
+ }
  
