6991da2cdd68b96be75d04ca0141524a53c08b9c
Srihari <120706989+Srihari1708@users.noreply.github.com>
2024-09-18 16:52:30 +0530

    Update Testchallenge.h

--- Testchallenge.h
+++ Testchallenge.h
@@ -1,4 +1,13 @@
 // void alertInCelcius(float farenheit);
 // int getfailurecount();
 
-int alertInCelsiusFacade(float farenheit);
+//Abstraction using class interface
+// functionpointer
+
+class INetworkAlerter{
+public:
+    virtual void alert(float celsius)=0;
+};
+
+
+int alertInCelsiusFacade(float farenheit *networkAlerterPtr);
