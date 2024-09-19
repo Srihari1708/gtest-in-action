d0b02af5994bb2e367cffaca6e2184baecd13d57
Srihari <120706989+Srihari1708@users.noreply.github.com>
2024-09-19 09:29:50 +0530

    Update TestChallenge.cpp

--- TestChallenge.cpp
+++ TestChallenge.cpp
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <assert.h>
 #include "Testchallenge.h"
- 
+
 int alertFailureCount = 0;
 
-int getFailureCount(){
-return alertFailureCount;
+int getFailureCount() {
+    return alertFailureCount;
 }
 
-void alertInCelcius(float farenheit,INetworkAlerter *networkAlerterPtr) {
+void alertInCelcius(float farenheit, INetworkAlerter* networkAlerterPtr) {
     float celcius = (farenheit - 32) * 5 / 9;
     int returnCode = networkAlerterPtr->alert(celcius);
+
+    // Modify this condition based on expected usage.
+    // Assuming the alert should return 200 for success, and anything else is a failure.
     if (returnCode != 200) {
         alertFailureCount += 1;
     }
 }
 
-int alertInCelciusFacade(float farenheit,INetworkAlerter *networkAlerterPtr){
-   alertInCelcius(farenheit,networkAlerterPtr);
-   return getFailureCount();
- }
- 
+int alertInCelciusFacade(float farenheit, INetworkAlerter* networkAlerterPtr) {
+    alertInCelcius(farenheit, networkAlerterPtr);
+    return getFailureCount();
+}
